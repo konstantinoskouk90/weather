745c1f85ba269a48663186c88a1265da7cfb72e3
konstantinoskouk90 <konstantinoskouk90@gmail.com>
2017-11-04 22:58:59 +0000

    Added print.js file and refactored part of the code.

---
+++ print.js
@@ -0,0 +1,22 @@
+// Capitalize first letter
+function capFirstLetter(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+// Convert to Celcius
+function kelvToCelc(kelv) {
+    return (kelv - 273.15).toFixed(1);
+}
+
+// Print weather message
+function printWeather(obj) {
+    console.log(`${
+        capFirstLetter(obj.weather[0].description)} in ${
+        obj.name}, ${
+        obj.sys.country} at the moment with a temperature of ${
+        kelvToCelc(obj.main.temp)} °C.`);
+}
+
+module.exports = {
+    printWeather: printWeather
+}
--- weather.js
+++ weather.js
@@ -1,42 +1,46 @@
-// Assign http module
+// Assign https module
 const https = require('https');
 
 // Retrieve API key
 const api = require('./api.json');
 
+// Require print functions
+const print = require('./print');
+
 // Define search options
 const search = {
-    city: "q",
-    zip: "zip"
+  city: "q",
+  zip: "zip"
 }
 
 // Check if input contains only numbers
 function isNumber(val) {
-   return /^\d+$/.test(val) ? search.zip : search.city;
+  return /^\d+$/.test(val) ? search.zip : search.city;
 }
 
 // Get current weather by using openweathermap.org API
 function get(args) {
 
-    // Number check
-    const params = isNumber(args);
-    
-    const request = https.get(`https://api.openweathermap.org/data/2.5/weather?${params}=${args}&APPID=${api.key}`, response => {
+  // Number check
+  const params = isNumber(args);
 
-        let body = "";
+  const request = https.get(`https://api.openweathermap.org/data/2.5/weather?${params}=${args}&APPID=${api.key}`, response => {
 
-        // Read data
-        response.on('data', chunk => {
-            body += chunk.toString();
-        });
+    let body = "";
 
-        // After the response has ended
-        response.on('end', () => {
-            console.log(body);
-            // Parse data
-            // Print data
-        });
+    // Read data
+    response.on('data', chunk => {
+      body += chunk.toString();
     });
+
+    // After the response has ended
+    response.on('end', () => {
+      // Parse data
+      const weather = JSON.parse(body); console.dir(weather);
+      // Print data
+      print.printWeather(weather);
+    });
+  });
 }
 
 module.exports.get = get;
